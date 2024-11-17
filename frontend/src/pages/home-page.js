60942b10f80952524be75b29443994693144cc6d
Lilian4441 <llhuang@wisc.edu>
2024-11-17 02:38:48 -0600

    integrating html page into js

--- frontend/src/pages/home-page.js
+++ frontend/src/pages/home-page.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function HomePage() {
   return (
-    <div as="home">
+    <div>
       <h1>Home</h1>
       <p>Welcome to MyApp. This is the home page.</p>
+      <iframe 
+        src="path/to/your/html-page.html" 
+        title="External HTML Page" 
+        style={{ width: '100%', height: '500px', border: 'none' }}
+      ></iframe>
     </div>
   );
 }
