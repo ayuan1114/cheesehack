c9d6aebf487bfd16a0fb3d064723b64aab926ee9
Felix <felixzhu120@gmail.com>
2025-05-30 17:05:17 -0400

    added comments to example.py

--- backend/example.py
+++ backend/example.py
@@ -1,10 +1,17 @@
 from data_extract import capture, save_vid, play_vid, extract_frames, save_norm_swing, load_norm_swing
 from model import process_vid, draw_pose_box, overlay_swing
 
+# extract frames from pro video
 pro_vid, width1, height1 = extract_frames("pro.mp4", get_dims=True)
+# save the pro video pose landmarks
 save_norm_swing(pro_vid, "pro_pose.csv")
+# load back the saved pro pose landmarks
 pro_pose = load_norm_swing("pro_pose.csv")
+# extract frames from user's swing video
 vid, width2, height2 = extract_frames("fswing.mp4", get_dims=True)
+# process the user's swing video for pose detection
 processed_vid, f_pose = process_vid(vid)
+# overlay the pro swing over the user's swing
 overlayed_frames = overlay_swing(vid, height2, width2, pro_pose, add_box=True, og_pose=True)
+# save video
 save_vid(overlayed_frames, "projected_vid.mp4", width2, height2)
